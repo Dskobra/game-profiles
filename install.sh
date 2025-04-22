d7ed117e7496fd531c95658b4919eec777320e38
Dskobra <jbttoms@gmail.com>
2025-04-22 18:49:34 -0500

    renamed pci.py to config_pci.py and redid it

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 mkdir "$HOME"/.config/MangoHud/
-python pci.py
+python config_pci.py
 chown "$USER":"$USER" *.conf
 cp *.conf "$HOME"/.config/MangoHud/
 git stash       # reset profiles after copying
