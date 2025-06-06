68de59780e78f3c5515ddead16574768609c294d
Dskobra <jbttoms@gmail.com>
2025-06-05 19:11:21 -0500

    removed scripts
    just keep the profiles for anyone that wants to use them otherwise I use
    dsks-tools to set them up on my pc

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-mkdir "$HOME"/.config/MangoHud/
-python config_pci.py
-chown "$USER":"$USER" *.conf
-cp *.conf "$HOME"/.config/MangoHud/
-git stash       # reset profiles after copying
